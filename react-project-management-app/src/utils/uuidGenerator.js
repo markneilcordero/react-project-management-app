9651f31e74d1909c308a7176cee3b6f303d117c5
Mark Neil <61368246+markneilcordero@users.noreply.github.com>
2025-06-24 04:08:50 +0800

    Delete react-project-management-app directory

--- react-project-management-app/src/utils/uuidGenerator.js
+++
@@ -1,9 +0,0 @@
-// src/utils/uuidGenerator.js
-
-/**
- * Generates a unique ID using timestamp and random suffix.
- * @returns {string} A unique string ID
- */
-export function generateId() {
-  return Date.now().toString(36) + Math.random().toString(36).substring(2, 6);
-}
